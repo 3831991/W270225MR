5b6c635ea4dac206e5dd93a24fcc94e4ecf0af1d
Your Name <you@example.com>
2025-06-26 15:56:42 +0300

    added

--- snake/script.js
+++ snake/script.js
@@ -1,5 +1,7 @@
+const speed = 150;
 const height = 30;
 const width = 30;
+let score = 0;
 const snake = [7, 6, 5, 4, 3, 2, 1, 0];
 let head = snake[0];
 const board = document.querySelector('.board');
@@ -21,10 +23,25 @@ function createBoard() {
 }
 
 function color() {
-    divs.forEach(div => div.classList.remove('active', 'head', 'up', 'right', 'down', 'left'));
+    divs.forEach(div => div.classList.remove('active', 'head', 'up', 'right', 'down', 'left', 'topLeftRadius', 'topRightRadius', 'bottomRightRadius', 'bottomLeftRadius'));
 
-    snake.forEach(num => {
+    snake.forEach((num, i) => {
         divs[num].classList.add('active');
+
+        const prev = snake[i + 1];
+        const next = snake[i - 1];
+
+        if (prev && next) {
+            if ((next == num - 1 && prev == num + width) || (next == num + width && prev == num - 1)) {
+                divs[num].classList.add('topLeftRadius');
+            } else if ((next == num + width && prev == num + 1) || (prev == num + width && next == num + 1)) {
+                divs[num].classList.add('topRightRadius');
+            } else if ((next == num + 1 && prev == num - width) || (prev == num + 1 && next == num - width)) {
+                divs[num].classList.add('bottomRightRadius');
+            } else if ((next == num - 1 && prev == num - width) || (prev == num - 1 && next == num - width)) {
+                divs[num].classList.add('bottomLeftRadius');
+            }
+        }
     });
 
     divs[head].classList.add('head', direction);
@@ -88,6 +105,9 @@ function move(dir) {
     snake.unshift(head);
 
     if (head == baitIndex) {
+        sound("eat_apple.mp3");
+        score += 10;
+        document.getElementById("score").innerText = score;
         setBait();
     } else {
         snake.pop();
@@ -97,14 +117,21 @@ function move(dir) {
     autoMove();
 }
 
+function sound(src) {
+    const audio = document.createElement("audio");
+    audio.src = src;
+    audio.play();
+}
+
 function autoMove() {
     clearInterval(interval);
-    interval = setInterval(() => move(direction), 200);
+    interval = setInterval(() => move(direction), speed);
 }
 
 function gameOver() {
     clearInterval(interval);
-    alert("איזה באסה...");
+    sound("error.mp3");
+    setTimeout(() => alert("איזה באסה..."), 50);
 }
 
 function setBait() {
