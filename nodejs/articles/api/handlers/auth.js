342b7c728c725ddd6c387ad84691ce9435d083aa
Your Name <you@example.com>
2025-06-29 11:34:15 +0300

    added

--- nodejs/articles/api/handlers/auth.js
+++ nodejs/articles/api/handlers/auth.js
@@ -1,14 +1,43 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { Router } from 'express';
 
-export const router = Router();
+const schema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    password: String,
+});
+
+const User = mongoose.model("users", schema);
+
+const router = Router();
 
 router.post('/login', (req, res) => {
 
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+    const { firstName, lastName, email, phone, password } = req.body;
 
+    const userFind = await User.findOne({ email });
+
+    if (userFind) {
+        return res.status(403).send({ message: "Email is used" })
+    }
+
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        phone,
+        password: await bcrypt.hash(password, 10),
+    });
+
+    const newUser = await user.save();
+
+    res.send(newUser);
 });
 
 export default router;
