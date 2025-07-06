a8faba4a099a96cb51117b4ee015bdf2301d1ac9
Your Name <you@example.com>
2025-07-06 11:53:21 +0300

    added

--- nodejs/files/backend/handlers/files.js
+++ nodejs/files/backend/handlers/files.js
@@ -1,9 +1,29 @@
 import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const router = Router();
 
-router.get('/', async (req, res) => {
-    res.send({});
+// שם הקובץ הנוכחי
+const __filename = fileURLToPath(import.meta.url);
+// שם התיקייה הנוכחית
+const __dirname = path.dirname(__filename);
+
+router.get('/', (req, res) => {
+    // שליחת מערך עם שמות הקבצים שיש בתיקייה
+    fs.readdir("./images", (err, files) => {
+        res.send(files);
+    });
+});
+
+router.get('/:fileName', (req, res) => {
+    // ניתוב אבסולוטי לקובץ
+    let url = `${__dirname}/../images/${req.params.fileName}`;
+    // ניקוי הנתיב מניתובים
+    url = path.resolve(url);
+    // שליחת הקובץ ללקוח
+    res.sendFile(url);
 });
 
 export default router;
