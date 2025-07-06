5a754645aa0f1c57f4aa9aef8670599d04278705
Your Name <you@example.com>
2025-07-06 10:30:44 +0300

    added

--- nodejs/articles/api/index.js
+++ nodejs/articles/api/index.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import AuthRouter from './handlers/auth.js';
 import ArticlesRouter from './handlers/articles.js';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import moment from 'moment/moment.js';
 
 // חיבור למסד הנתונים
 await mongoose.connect('mongodb://127.0.0.1:27017/full-stack-W270225MR');
@@ -22,6 +25,43 @@ app.use(cors({
     methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
 }));
+
+app.use((req, res, next) => {
+    // אם הניתוב פונה לחידוש טוקן, לא הוספנו לתוך התיעוד (כי יש יותר מדי פניות כאלו)
+    if (req.url === '/token') {
+        next();
+        return;
+    }
+
+    // מביא לנו את פרטי היוזר שמחובר
+    const data = jwt.decode(req.headers.authorization);
+
+    // יצרנו שם דינאמי לקובץ הלוגים, ע"מ שכל יום יהיה קובץ אחר
+    const fileName = `./logs/log_${moment().format("YYYY_MM_DD")}.txt`;
+    let content = "";
+
+    content += `Time: ${moment().format("DD/MM/YYYY HH:mm:ss")}\n`;
+    content += `Method: ${req.method}\n`;
+    content += `Route: ${req.url}\n`;
+
+    // אם יש יוזר מחובר (כלומר, אם יש טוקן)
+    if (data?.userId) {
+        content += `UserID: ${data.userId}\n`;
+        content += `UserName: ${data.fullName}\n`;
+    }
+
+    // הוספת שורות חדשות ועוד קישוטים
+    content += '\n---\n\n';
+
+    // אם לא קיימת תיקייה של לוגים - הוא מייצר אותה
+    fs.mkdirSync('./logs', { recursive: true });
+
+    // אם אין את הקובץ הוא מייצר אותו, אחרת - הוא מוסיף אליו את התוכן
+    fs.appendFile(fileName, content, err => {});
+
+    // עובר ל-MiddleWare הבא
+    next();
+});
 
 // הפעלת השרת ושימוש בפורט
 app.listen(3500, () => {
