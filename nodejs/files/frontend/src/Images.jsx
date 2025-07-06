1c6e2f77b7a513a49383158a33b5c0dff5e882f7
Your Name <you@example.com>
2025-07-06 14:29:18 +0300

    added

--- nodejs/files/frontend/src/Images.jsx
+++ nodejs/files/frontend/src/Images.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Images() {
     const [images, setImages] = useState([]);
+    const fileInput = useRef(null);
 
     useEffect(() => {
         getImages();
@@ -15,17 +16,41 @@ export default function Images() {
         }
     }
 
+    const fileChange = async ev => {
+        const file = ev.target.files[0];
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const res = await fetch("http://localhost:3333/files/upload", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (res.ok) {
+            getImages();
+        }
+    }
+
     return (
         <div className="Images">
             {
                 images.map(imageName => 
-                    <div className="imageFrame">
-                        <div className="image">
-                            <img src={`http://localhost:3333/files/${imageName}`} />
+                    <a key={imageName} href={`http://localhost:3333/files/${imageName}`} target="_blank">
+                        <div className="imageFrame">
+                            <div className="image">
+                                <img src={`http://localhost:3333/files/${imageName}`} />
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 )
             }
+            <div className="uploadFrame">
+                {/* חייבים את האלמנט הזה בכדי לייבא קובץ */}
+                {/* היות והוא מכוער, הסתרנו אותו והשתמשו בלחצן שיפעיל אותו */}
+                <input type="file" ref={fileInput} onChange={fileChange} />
+                <button onClick={() => fileInput.current.click()}>בחר תמונה..</button>
+            </div>
         </div>
     )
 }
