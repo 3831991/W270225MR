eb5f44905ab317886a2c8da7377d86eebd005dab
Your Name <you@example.com>
2025-07-15 11:23:10 +0300

    added

--- nodejs/employee/api/handlers/employee.js
+++ nodejs/employee/api/handlers/employee.js
@@ -1,5 +1,32 @@
-import mongoose from "mongoose";
+import { model, Schema } from "mongoose";
 import { Router } from 'express';
+
+const Address = new Schema({
+    city: String,
+    street: String,
+    house: String,
+});
+
+const Image = new Schema({
+    name: String,
+    size: Number,
+    type: String,
+});
+
+const EmployeeSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    personalId: String,
+    phone: String,
+    email: String,
+    birthDate: Date,
+    address: Address,
+    image: Image,
+    gender: String,
+    userId: Schema.Types.ObjectId,
+});
+
+export const Employee = model("employees", EmployeeSchema);
 
 const router = Router();
 
