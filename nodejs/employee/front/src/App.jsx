14207553f57197018ac59ae8f807e25beca46fd1
Your Name <you@example.com>
2025-07-15 11:44:02 +0300

    added

--- nodejs/employee/front/src/App.jsx
+++ nodejs/employee/front/src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Employee from './Employee/Employee'
 
 function App() {
   return (
     <>
       <h1>ניהול עובדים</h1>
+
+      <Routes>
+        <Route path="/" element={<Employee />} />
+      </Routes>
     </>
   )
 }
