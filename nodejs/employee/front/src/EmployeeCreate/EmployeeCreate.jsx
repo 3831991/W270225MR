ad4b508273acfb96f5b10237e0016fc8151f807e
Your Name <you@example.com>
2025-07-17 11:43:55 +0300

    addded

--- nodejs/employee/front/src/EmployeeCreate/EmployeeCreate.jsx
+++ nodejs/employee/front/src/EmployeeCreate/EmployeeCreate.jsx
@@ -2,6 +2,8 @@ import './EmployeeCreate.css';
 
 export default function EmployeeCreate() {
     return (
-        <div style={{ color: 'white', fontSize: '3em' }}>EmployeeCreate</div>
+        <div>
+            
+        </div>
     )
 }
