4ebd1d83c1f7b72901d2f9fc880363d9ba97f137
Your Name <you@example.com>
2025-07-24 14:18:15 +0300

    added

---
+++ breakout/src/Game/Game.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './Game.css'
+import { useEffect } from 'react';
+const bricksAmount = 50;
+
+export default function Game() {
+    const [bricks, setBricks] = useState([]);
+
+    useEffect(() => {
+        const arr = [];
+
+        for (let i = 0; i < bricksAmount; i++) {
+            arr.push(i);
+        }
+
+        setBricks(arr);
+    }, []);
+
+    return (
+        <div className='Game'>
+            <header>
+                {bricks.map(x => 
+                    <div className='Brick'></div>
+                )}
+            </header>
+        </div>
+    )
+}
