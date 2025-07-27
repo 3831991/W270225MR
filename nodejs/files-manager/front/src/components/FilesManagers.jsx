ca933aa38c751034b6defec9e6b39a202ae626be
Your Name <you@example.com>
2025-07-27 10:29:11 +0300

    added

--- nodejs/files-manager/front/src/components/FilesManagers.jsx
+++ nodejs/files-manager/front/src/components/FilesManagers.jsx
@@ -1,23 +1,50 @@
+import { useState } from "react";
+import { icons } from "../config";
+
 export default function FilesManagers() {
-    const files = [
+    const [files, setFiles] = useState([
         { fileName: 'תיקיה ראשונה', isFolder: true },
         { fileName: 'תיקיה שנייה', isFolder: true },
         { fileName: 'תיקיה שלישית', isFolder: true },
         { fileName: 'תיקיה רביעית', isFolder: true },
         { fileName: 'תיקיה חמישית', isFolder: true },
-        { fileName: 'תיקיה שישית', isFolder: true },
-        { fileName: 'תיקיה שביעית', isFolder: true },
-    ];
-    
+        { fileName: 'img4.jpg', type: 'jpg', isFolder: false },
+        { fileName: 'logo.png', type: 'png', isFolder: false },
+        { fileName: 'animation.gif', type: 'gif', isFolder: false },
+        { fileName: 'vector.svg', type: 'svg', isFolder: false },
+        { fileName: 'document.pdf', type: 'pdf', isFolder: false },
+        { fileName: 'notes.txt', type: 'plain', isFolder: false },
+        { fileName: 'archive.zip', type: 'zip', isFolder: false },
+    ]);
+
+    const createFolder = () => {
+        const folderName = prompt("בחר שם לתיקייה");
+
+        if (!folderName) {
+            return;
+        }
+
+        setFiles([...files, { fileName: folderName, isFolder: true }]);
+    }
+
     return (
         <div>
             <h1>ניהול קבצים</h1>
 
+            <div className="actions">
+                <button className='button' onClick={createFolder}><i className='fa fa-plus'></i> תיקייה חדשה</button>
+                <button className='button'><i className='fa fa-upload'></i> העלאת קבצים</button>
+            </div>
+
             <div className="files">
                 {
-                    files.map(f => 
+                    files.map(f =>
                         <div className="file">
-                            <i className="fa fa-folder"></i>
+                            {
+                                f.isFolder ?
+                                    <i className="fa fa-folder"></i> :
+                                    <i className={'fa fa-' + (icons[f.type] || 'file')}></i>
+                            }
                             <p>{f.fileName}</p>
                         </div>
                     )
