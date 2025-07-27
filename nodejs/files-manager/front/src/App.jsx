23e8dcaf929633d8665d390f3d16e9ff700d39aa
Your Name <you@example.com>
2025-07-27 09:45:56 +0300

    added

---
+++ nodejs/files-manager/front/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FilesManagers from './components/FilesManagers'
+
+function App() {
+
+  return (
+    <>
+      <FilesManagers />
+    </>
+  )
+}
+
+export default App
---
+++ nodejs/files-manager/front/src/components/FilesManagers.jsx
@@ -0,0 +1,5 @@
+export default function FilesManagers() {
+    return (
+        <div>ניהול קבצים</div>
+    )
+}
