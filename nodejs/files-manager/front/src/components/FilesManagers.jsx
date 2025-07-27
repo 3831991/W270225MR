1bbccf0dd088e3c7af95d7f48d3e43a5910d76b4
Your Name <you@example.com>
2025-07-27 09:53:58 +0300

    added

--- nodejs/files-manager/front/src/components/FilesManagers.jsx
+++ nodejs/files-manager/front/src/components/FilesManagers.jsx
@@ -1,5 +1,28 @@
 export default function FilesManagers() {
+    const files = [
+        { fileName: 'תיקיה ראשונה', isFolder: true },
+        { fileName: 'תיקיה שנייה', isFolder: true },
+        { fileName: 'תיקיה שלישית', isFolder: true },
+        { fileName: 'תיקיה רביעית', isFolder: true },
+        { fileName: 'תיקיה חמישית', isFolder: true },
+        { fileName: 'תיקיה שישית', isFolder: true },
+        { fileName: 'תיקיה שביעית', isFolder: true },
+    ];
+    
     return (
-        <div>ניהול קבצים</div>
+        <div>
+            <h1>ניהול קבצים</h1>
+
+            <div className="files">
+                {
+                    files.map(f => 
+                        <div className="file">
+                            <i className="fa fa-folder"></i>
+                            <p>{f.fileName}</p>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     )
 }
